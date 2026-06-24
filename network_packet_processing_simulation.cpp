b11ee3cc7114b9bfc3f765e20b5634cb9aefb3c0
robi872 <122002085+robi872@users.noreply.github.com>
2026-06-24 13:51:49 +0800

    Update network_packet_processing_simulation.cpp

--- network_packet_processing_simulation.cpp
+++ network_packet_processing_simulation.cpp
@@ -5,37 +5,40 @@ typedef long long int ll;
 #define MOD 1000000007
 int main() 
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	ll s,n,i,c=0,a,p,t=0;
-	queue <ll> q;
-	cin>>s>>n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll s, n, i, c = 0, a, p, t = 0;
+    queue <ll> q;
+    cin >> s >> n;
+
     for (i = 0; i < n; i++) 
     {
-    cin >> a >> p;
-    while (!q.empty() && q.front() <= a) 
-    {
-        q.pop();
-    }
-    if (q.size() == s) 
-    {
-        cout << -1 << "\n";
-    } 
-    else 
-    {
-        if (q.empty()) 
+        cin >> a >> p;
+        
+        while (!q.empty() && q.front() <= a) 
         {
-            cout << a << "\n";
-            t = a + p;
+            q.pop();
+        }
+        
+        if (q.size() == s) 
+        {
+            cout << -1 << "\n";
         } 
         else 
         {
-            cout << t << "\n";
-            t = t + p;
+            if (q.empty()) 
+            {
+                cout << a << "\n";
+                t = a + p;
+            } 
+            else 
+            {
+                cout << t << "\n";
+                t = t + p;
+            } 
+            q.push(t);
         } 
-        q.push(t);
-            }
-        }
-	}
-	return 0;
-}
+    } 
+    return 0;
+} 
